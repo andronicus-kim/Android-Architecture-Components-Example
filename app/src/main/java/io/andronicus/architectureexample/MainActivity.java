4488da551ca735d81ef56fce235d411c34bac353
andronicus-kim <androkim.kim7@gmail.com>
2018-11-29 15:49:28 +0300

    instantiates our view model in UI controller

--- app/src/main/java/io/andronicus/architectureexample/MainActivity.java
+++ app/src/main/java/io/andronicus/architectureexample/MainActivity.java
@@ -1,13 +1,20 @@
 package io.andronicus.architectureexample;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NoteViewModel mViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get Viewmodel from the android system since it knows when to
+        // create a new one when necessary
+        mViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
     }
 }
