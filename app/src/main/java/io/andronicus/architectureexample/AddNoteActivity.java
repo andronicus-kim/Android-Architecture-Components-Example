a0c769c51bba54de116223bd26829d508620c62f
andronicus-kim <androkim.kim7@gmail.com>
2018-11-29 18:00:40 +0300

    some changes

--- app/src/main/java/io/andronicus/architectureexample/AddNoteActivity.java
+++ app/src/main/java/io/andronicus/architectureexample/AddNoteActivity.java
@@ -2,8 +2,11 @@ package io.andronicus.architectureexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -24,5 +27,34 @@ public class AddNoteActivity extends AppCompatActivity {
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
         setTitle("Add Note");
+    }
+
+    private void saveNote(){
+        String title = mEditTextTitle.getText().toString().trim();
+        String description = mEditTextDescription.getText().toString().trim();
+        int priority = mNumberPicker.getValue();
+
+        if (title.isEmpty() || description.isEmpty()){
+            Toast.makeText(this, "Please input Title and Description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_note_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.save_note){
+            saveNote();
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
